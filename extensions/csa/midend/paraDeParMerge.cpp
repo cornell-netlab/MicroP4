942cf15e2d0d123cd85044b3626803dd5a44283a
Ryan Doenges <me@ryandoeng.es>
2020-01-23 17:44:08 -0500

    sketch traversal order for merger

--- extensions/csa/midend/paraDeParMerge.cpp
+++ extensions/csa/midend/paraDeParMerge.cpp
@@ -8,6 +8,14 @@
 namespace CSA {
 
 const IR::Node* ParaParserMerge::preorder(IR::P4Parser* p4parser) {
+    auto start_state1 = p4parser->states.getDeclaration<IR::ParserState>("start");
+    auto start_state2 = p2->states.getDeclaration<IR::ParserState>("start");
+    if (start_state1 != nullptr && start_state2 != nullptr) {
+        currP2State = start_state2;
+        visit(start_state1);
+    } else {
+        /* fail */
+    }
     return p4parser;
 }
 const IR::Node* ParaParserMerge::postorder(IR::P4Parser* p4parser) {
@@ -15,6 +23,33 @@ const IR::Node* ParaParserMerge::postorder(IR::P4Parser* p4parser) {
 }
 
 const IR::Node* ParaParserMerge::preorder(IR::ParserState* state) {
+    /* State
+     * - state                the state in p1 we're looking at
+     * - currp2state          the state in p2 we're looking at
+     * - hdr_map1, hdr_map2   where we've put header types
+     * - new_parser           the merged parser
+     */
+
+    /* 
+     * States s1, s2 each extract one header h1, h2.
+     *
+     * Check that h1 is equivalent to h2 (same size, or same # of fields and
+     * types of fields, or whatever).
+     *
+     * Look at the transitions taken by s1, s2.
+     *
+     * If they are both doing an unconditional transition to new states t1, t2,
+     * then visit (t1, t2).
+     * 
+     * If they are selecting on different things, ??
+     *
+     * If they are selecting on the same thing h.f, match up the cases.
+     * 
+     * For each case COND => s1', s2' that matches, visit (s1, s2).
+     *
+     * For any cases with no match, copy the states and all their descendants
+     * into the merged parser.
+     */
     return state;
 }
 const IR::Node* ParaParserMerge::postorder(IR::ParserState* state) {
