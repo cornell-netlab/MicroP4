87b5fd462198c6832752b273f100b94a5588e5bf
hksoni <hardiksk@gmail.com>
2020-06-26 18:17:56 -0400

    router_ipv4v6srv6_main_tna

--- extensions/csa/msa-examples/run_up4_v1model.sh
+++ extensions/csa/msa-examples/run_up4_v1model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# PROGRAMS="routerv4_main routerv6_main routerv46_main"
-# PROGRAMS="router_ipv4v6_nat_acl"
-PROGRAMS="routerv4_main routerv6_main routerv46_main router_ipv4v6_nat_acl"
+# PROGRAMS="routerv4_main routerv6_main routerv46_main router_ipv4v6_nat_acl router_ipv4v6srv6_main routerv46lrx_main router_ipv4srv4ipv6_main"
+PROGRAMS="router_ipv4v6srv6_main"
+# PROGRAMS="routerv4_main routerv6_main routerv46_main router_ipv4v6_nat_acl"
 
 BMV2_MININET_PATH=./bmv2/mininet/msa-examples
 BMV2_SIMPLE_SWITCH_BIN=./bmv2/targets/simple_switch/simple_switch
